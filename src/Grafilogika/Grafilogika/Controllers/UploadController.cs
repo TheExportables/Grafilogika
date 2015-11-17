bb79bfc4df2d4e2af7b9f4c25ff9817f4430bd2e
Tamás Ferenc Tóth <tarotth@sch.bme.hu>
2015-11-17 20:18:57 +0100

    Tomi viewok

--- src/Grafilogika/Grafilogika/Controllers/UploadController.cs
+++ src/Grafilogika/Grafilogika/Controllers/UploadController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -10,6 +11,45 @@ namespace Grafilogika.Controllers
     {
         public ActionResult Upload()
         {
+            return View();
+        }
+
+        public ActionResult CreateOwnGame()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult CreateOwnGameProcess()
+        {
+            return View();
+        }
+
+        public ActionResult UploadGame()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult UploadGameProcess(HttpPostedFileBase file)
+        {
+            if (file != null && file.ContentLength > 0)
+            {
+                var fileName = Path.GetFileName(file.FileName);
+                var path = Path.Combine(Server.MapPath("~/Upload/"), fileName);
+                if (!Directory.Exists(Server.MapPath("~/Upload")))
+                {
+                    Directory.CreateDirectory(Server.MapPath("~/Upload"));
+                }
+                file.SaveAs(path);
+            }
+
+            //TODO ascii feldolgozás
+
+            //TODO adatbázisba mentés
+
+            //TODO upload mappa törlése
+
             return View();
         }
     }
---
+++ src/Grafilogika/Grafilogika/Models/FieldModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Grafilogika.Models
+{
+    public class FieldModel
+    {
+        public int Rows;
+        public int Columns;
+    }
+}
