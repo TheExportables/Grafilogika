e9d45dd7d0e49c518795bfd7c0b81d5721c364f2
Ameshin <bsony17@gmail.com>
2015-11-18 17:12:21 +0100

    Mark gamecontroller

---
+++ src/Grafilogika/Grafilogika/Controllers/GameController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Grafilogika.Controllers
+{
+    public class GameController : Controller
+    {
+        public ActionResult Game()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult CheckGameSolution(String gameName, String gameSolution) {
+
+            //TODO játékok adatbázisból get a gameName-el egyenlő nevűt, aztán 
+            //if gameSolution.Equals(adatbázisban levő megoldás) akkor
+            //return faszavagy; else return elbasztad.
+
+            //adatbázisba mentés a hibát, wint majd az értékeléskor adjuk hozzá
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult PostGameRating(int rating) {
+
+            //TODO adatbázisból getRating és getWins. ++Wins; Rating+=rating; Rating/=Wins;
+
+            return View();
+        }
+    }
+}
